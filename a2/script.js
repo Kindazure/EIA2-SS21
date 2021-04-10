15f1bf26a38c12a5e793708c634a7bba99b94a3b
Kindazure <larissa.isabel.gaede@hs-furtwangen.de>
2021-04-10 12:30:28 +0200

    fixed ts file

--- a2/script.js
+++ a2/script.js
@@ -1,30 +1,33 @@
 "use strict";
 var Boxes;
 (function (Boxes) {
-    let n = 5;
-    let color;
-    let x = 0;
-    let y = 0;
-    for (let i = 0; i < n; i++) {
-        y += (i == 2) ? 20 : 50;
-        x = (x + 170) % 400;
-        switch (i) {
-            case 0:
-                color = "#ff0000";
-                break;
-            case 1:
-            case 4:
-                color = "#00ff00";
-                break;
-            case 3:
-                continue;
-            default:
-                color = "#0000ff";
-        }
-        for (let size of ["big", "medium", "small"]) {
-            createBox(color, x, y, size);
-            if (i == 4)
-                break;
+    window.addEventListener("load", box);
+    function box() {
+        let n = 5;
+        let color;
+        let x = 0;
+        let y = 0;
+        for (let i = 0; i < n; i++) {
+            y += (i == 2) ? 20 : 50;
+            x = (x + 170) % 400;
+            switch (i) {
+                case 0:
+                    color = "#fcba03";
+                    break;
+                case 1:
+                case 4:
+                    color = "#0398fc";
+                    break;
+                case 3:
+                    continue;
+                default:
+                    color = "#b66bcf";
+            }
+            for (let size of ["big", "medium", "small"]) {
+                createBox(color, x, y, size);
+                if (i == 4)
+                    break;
+            }
         }
     }
     function createBox(_color, _x, _y, _size) {
