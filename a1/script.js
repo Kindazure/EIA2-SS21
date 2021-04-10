1b580a42327e9d7ff5d5454a78d98ef499b31d1e
Kindazure <larissa.isabel.gaede@hs-furtwangen.de>
2021-04-10 12:27:41 +0200

    a2

--- a1/script.js
+++ a1/script.js
@@ -1,19 +1,24 @@
 "use strict";
 var RandomPoems;
 (function (RandomPoems) {
+    // filling arrays
     var avalanche = ["Cloud ", "Tifa ", "Aerith ", "Barret ", "Red ", "Jessie ", "Biggs ", "Wedge "];
     var mission = ["searches for ", "battles ", "destroys ", "blows up ", "crushes ", "fights ", "sneaks by ", "kills "];
     var enemies = ["Sephiroth", "Shinra", "Don Corneo", "Zack ", "Rude ", "Hojo ", "Scarlet ", "Palmer "];
     var i;
+    // for-loop connected to the function, loops through the random numbers
     for (i = avalanche.length; i > 0; i--) {
         var ranum = getVerse(avalanche, mission, enemies);
         console.log(ranum);
     }
     function getVerse(_avalanche, _mission, _enemies) {
+        // generating random numbers from 0-7
         let ranum1 = Math.floor(Math.random() * avalanche.length);
         let ranum2 = Math.floor(Math.random() * mission.length);
         let ranum3 = Math.floor(Math.random() * enemies.length);
+        // combining three random numbers to complete a verse
         let verse = _avalanche[ranum1] + _mission[ranum2] + _enemies[ranum3];
+        // splicing to avoid doubles
         _avalanche.splice(ranum1, 1);
         _mission.splice(ranum2, 1);
         _enemies.splice(ranum3, 1);
