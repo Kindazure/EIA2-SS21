d371237eaec7ee947826b9f1fc69035a14c4fa3c
Kindazure <larissa.isabel.gaede@hs-furtwangen.de>
2021-06-14 22:56:46 +0200

    added sources

--- a11/script.ts
+++ a11/script.ts
@@ -1,4 +1,12 @@
 namespace eia10_2 {
+     /* 
+    Aufgabe: L10.2_Blumenwiese: Polymorphie
+    Name: Larissa Gaede
+    Datum: 14.06.2021
+    Quellen: Eva Breuninger, Maximilian Buckel
+    Ehrenmann: Sören Winterhalder
+    */
+
     let timeScale: number = 0.005;
     let scene: Scene = new Scene(timeScale);
 
